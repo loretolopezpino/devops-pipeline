0e4082a69afe2e4e1b06323fcdb04fcc229c0372
loretolopezpino <loreto.lopezpino@gmail.com>
2023-03-16 19:41:23 -0300

    Fix

--- products/src/index.js
+++ products/src/index.js
@@ -1,6 +1,6 @@
 import express from 'express'; 
 //const express = require("express") versiónnueva "type": "module" en package.json
-import randomBytes from 'crypto';
+import { randomBytes } from 'crypto';
 import bodyParser from 'body-parser'
 
 const app = express()
@@ -15,12 +15,12 @@ app.get('/products', (req, res) => {
 app.post('/products', (req, res) => {
     const id = randomBytes(4).toString('hex');
 
-    const {name} = reg.body
+    const {name} = req.body
     products[id] = {
         id,
         name
     }
-    res.status.send(products[id])
+    res.status(201).send(products[id])
 });
 
 app.listen(3000, () => {
